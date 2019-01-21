5e00b21ab4d567f034a323c36e2503b65823edb0
nighrain <wangzsjob@163.com>
2019-01-21 16:39:04 +0800

    初始提交0.1

---
+++ src/main/java/com/tensquare/TensquareParentApplication.java
@@ -0,0 +1,14 @@
+package com.tensquare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TensquareParentApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TensquareParentApplication.class, args);
+  }
+
+}
+
